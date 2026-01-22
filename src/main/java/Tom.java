0b8f0f6fab326e9840ec96052d8c9564b41e7636
Qiao Yanming <e1482084@u.nus.edu>
2026-01-23 00:59:57 +0800

    Complete Level-0 Rename, Greet, Exit

---
+++ src/main/java/Tom.java
@@ -0,0 +1,10 @@
+public class Tom {
+    public static void main(String[] args) {
+        String name = "Tom";
+        //Greet the user
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        //Exits
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
