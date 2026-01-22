734e26f2cd48426a28f0456aa5af30ef17b72f6b
Qiao Yanming <e1482084@u.nus.edu>
2026-01-23 01:15:42 +0800

    Complete Level-1: Echo

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
 public class Tom {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String name = "Tom";
         //Greet the user
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        //Exits
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while(true){
+            String User_input = scanner.nextLine();
+            if (User_input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(User_input);
+        }
+        scanner.close();
     }
 }
